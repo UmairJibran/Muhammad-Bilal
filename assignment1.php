96d3090988136e25d0efb6be276a88ce32439bab
Umair Jibran <umairjibran7@gmail.com>
2020-07-27 15:02:35 +0500

    assignment 1

---
+++ assignment1.php
@@ -0,0 +1,13 @@
+<?php
+   $string = "This is String";
+   $integer = 1452;
+   $float = 3.14;
+   $boolean = true;
+   var_dump($string);
+   echo "<br>";
+   var_dump($integer);
+   echo "<br>";
+   var_dump($float);
+   echo "<br>";
+   var_dump($boolean);
+?>
